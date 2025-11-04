ce174041ee630dc204721cb09ea588bd4ec5a423
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-11-04 18:34:59 +0200

    V04

---
+++ Store.Project.Persistence/SpecificationsEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Store.Project.Domain.Contracts;
+using Store.Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Project.Persistence
+{
+    public static class SpecificationsEvaluator
+    {
+        public static IQueryable<TEntity> GetQuery<TKey,TEntity>(IQueryable<TEntity> inputQuery,ISpecifications<TKey,TEntity> spec) where TEntity : BaseEntity<TKey>
+        {
+            var query = inputQuery;
+            if(spec.Criteria is not null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+            query = spec.Include.Aggregate(query, (query, IncludeExpression) => query.Include(IncludeExpression));
+
+            return query;
+        }
+    }
+}
